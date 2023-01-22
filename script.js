7e5066160a044efe7fc1c7fc990f63879c338b12
Neeraj kumar <104791048+neeku06dz@users.noreply.github.com>
2023-01-22 05:59:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function fun() {
+    var mode = document.querySelector(".container");
+    mode.classList.toggle("active");
+}
+function like() {
+    var like = document.querySelector(".last_icon");
+    like.classList.add("click");
+}
